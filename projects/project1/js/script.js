30112312d6eca557324c0452597734a8b11eb5e3
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-02-07 15:48:16 -0500

    P1: Started the elevator UI, added buttons with numbers that light up upon activation (click)

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,9 +1,50 @@
+"use strict";
 /*****************
 
-Title of Project
-Author Name
+Sisyphus' meeting
+Melissa Lim
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Represent the story of Sisyphus on his way to his meeing with Zeus
+in order to receive the decision to his punishment.
+A long elevator experience.
 
 ******************/
+// Store jQuery selection of all buttons
+let $buttons;
+// Store floor number counter
+let floorNum = 1;
+
+// When the document is loaded we call the setup function
+$(document).ready(setup);
+
+// setup()
+//
+// Sets the handler for floor buttons click and floor level reached
+function setup() {
+  // Save the selection of all buttons
+  $buttons = $("#button");
+  // Makes buttons selectable
+  $buttons.selectable();
+  // Allow for multiple selection
+  $buttons.on("selectablestart", function (event, ui) {
+    event.originalEvent.ctrlKey = true;
+  });
+  // Set a click handler on the buttons
+  $buttons.on('click',buttonClicked);
+  // Set in interval to update the floor level
+  setInterval(update,5000);
+}
+
+// buttonClicked()
+//
+//
+function buttonClicked() {
+
+}
+
+// update()
+//
+//
+function update() {
+
+}
