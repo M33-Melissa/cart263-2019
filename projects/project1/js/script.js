a946261d48c8f3b81706f3722c0e5e8b8b35d2ef
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-02-08 17:40:17 -0500

    P1: Added floor number and door sliding on each floor

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,18 +1,22 @@
 "use strict";
 /*****************
 
-Sisyphus' meeting
+Sisyphus' Meeting
 Melissa Lim
 
 Represent the story of Sisyphus on his way to his meeing with Zeus
 in order to receive the decision to his punishment.
-A long elevator experience.
+Zeus' office is on the last floor. With floor levels that keeps appearing.
+Therefore, resulting a long elevator experience.
 
 ******************/
 // Store jQuery selection of all buttons
 let $buttons;
+let $floorNums;
 // Store floor number counter
-let floorNum = 1;
+let floorNum = 2;
+let numFloors = 20;
+let interval;
 
 // When the document is loaded we call the setup function
 $(document).ready(setup);
@@ -23,28 +27,40 @@ $(document).ready(setup);
 function setup() {
   // Save the selection of all buttons
   $buttons = $("#button");
+  $floorNums = $("#floorNum");
   // Makes buttons selectable
-  $buttons.selectable();
+  $buttons.selectable({
+    tolerance: "fit"
+  });
   // Allow for multiple selection
   $buttons.on("selectablestart", function (event, ui) {
     event.originalEvent.ctrlKey = true;
   });
   // Set a click handler on the buttons
-  $buttons.on('click',buttonClicked);
+  $buttons.click(buttonClicked);
   // Set in interval to update the floor level
-  setInterval(update,5000);
+  interval = setInterval(update,10000);
 }
 
 // buttonClicked()
 //
 //
 function buttonClicked() {
-
+  console.log("cllicked");
 }
 
 // update()
 //
 //
 function update() {
+  $floorNums.text(floorNum);
+  floorNum++;
+  $("#doors").animate({left: "60%"},1000,function() {});
+  $("#doors").delay(1000).animate({left: "0%"},1000,function(){});
+}
 
+// updateButton()
+//
+// Randomly activates a button
+function updateButton() {
 }
