13eee2245b1c44622b1418433e50f0f014e7eb1b
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-02-08 23:59:02 -0500

    P1: Added images and gifs to appear at random behind elevator door

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -10,13 +10,17 @@ Zeus' office is on the last floor. With floor levels that keeps appearing.
 Therefore, resulting a long elevator experience.
 
 ******************/
-// Store jQuery selection of all buttons
+// Store jQuery selection of all buttons, spans, and floor numbers
 let $buttons;
+let $spans;
 let $floorNums;
-// Store floor number counter
+
+// Store floor number counter values
 let floorNum = 2;
 let numFloors = 20;
-let interval;
+
+// Store door opening interval
+let doorInterval;
 
 // Store sound effects
 let buttonSFX = new Audio("assets/sounds/button.mp3");
@@ -31,7 +35,7 @@ $(document).ready(setup);
 //
 // Sets the handler for floor buttons click and floor level reached
 function setup() {
-  // Music playing
+  // Play background music
   elevatorMusic.play();
   elevatorMusic.loop = true;
   elevatorMusic.volume = 0.3;
@@ -45,42 +49,116 @@ function setup() {
     tolerance: "fit"
   });
 
-  // Allow for multiple selection
+  // Allow for multiple button selection
   $buttons.on("selectablestart", function (event, ui) {
     event.originalEvent.ctrlKey = true;
   });
 
   // Set a click handler on the buttons
-  $("span").click(buttonClicked);
+  $spans = $("span");
+  $spans.on('click',buttonClicked);
 
   // Set in interval to update the floor level
-  interval = setInterval(update,10000);
+  doorInterval = setInterval(update,7000);
 }
 
 // buttonClicked()
 //
-//
+// On button click, SFX plays and another floor button is added
 function buttonClicked() {
 
-  buttonSFX.play();
-  buttonSFX.playbackRate = 3.5;
-  console.log("cllicked");
+  // Play button click SFX
+  if (!buttonSFX.isPlaying) {
+    buttonSFX.play();
+    buttonSFX.playbackRate = 3.5;
+  }
+
+  // Increment floor number, add buttons every click
+  numFloors++;
+  $("<span class=\"ui-state-default\">"+numFloors+"</span>").insertAfter("p");
+  $("span").on('click',buttonClicked);
 }
 
 // update()
 //
-//
+// Animates door opening, adding floor number
 function update() {
-  $floorNums.text(floorNum);
-  floorNum++;
-  $("#doors").animate({left: "60%"},1000,function() {});
-  $("#doors").delay(2000).animate({left: "0%"},1000,function(){});
+  // Arrived to floor level SFX
   floorSFX.play();
   floorSFX.volume = 0.5;
+
+  // Change floor number and adds one for next
+  $floorNums.text(floorNum);
+  floorNum++;
+
+  // Door opening and closing animation
+  $("#doors").animate({left: "60%"},1000,function() {});
+  $("#doors").delay(2000).animate({left: "0%"},1000,function(){});
+
+  // Randomize images appearing behind doors
+  if(floorNum > 3) {
+    randImage();
+  }
 }
 
-// updateButton()
+// randImage()
 //
-// Randomly activates a button
-function updateButton() {
+// Randomizes image that appears behind elevator doors
+function randImage() {
+
+  let randNum = Math.random();
+
+  if(randNum > 0.9) {
+
+    $("#behindDoor img").attr("src", "assets/images/djkhaled.gif");
+    $("#behindDoor img").attr("width", "200%");
+
+  } else if(randNum > 0.8) {
+
+    $("#behindDoor img").attr("src", "assets/images/surprised-pikachu.png");
+    $("#behindDoor img").attr("width", "75%");
+
+  } else if(randNum > 0.7) {
+
+    $("#behindDoor img").attr("src", "assets/images/thumbs-up.gif");
+    $("#behindDoor img").attr("width", "300%");
+
+  } else if(randNum > 0.6) {
+
+    $("#behindDoor img").attr("src", "assets/images/sisyphus-dwarves.jpg");
+    $("#behindDoor img").attr("width", "120%");
+
+  } else if(randNum > 0.5) {
+
+    $("#behindDoor img").attr("src", "assets/images/gnomed.png");
+    $("#behindDoor img").attr("width", "400%");
+    $("body").css("background-color","black");
+
+  } else if(randNum > 0.4) {
+
+    $("#behindDoor img").attr("src", "assets/images/overwork.png");
+    $("#behindDoor img").attr("width", "65%");
+
+  } else if(randNum > 0.3) {
+
+    $("#behindDoor img").attr("src", "assets/images/prairiedog-office.png");
+    $("#behindDoor img").attr("width", "150%");
+
+  } else if(randNum > 0.2) {
+
+    $("#behindDoor img").attr("src", "assets/images/pleiades.jpg");
+    $("#behindDoor img").attr("width", "70%");
+
+  } else if(randNum > 0.1) {
+
+    $("#behindDoor img").attr("src", "assets/images/isabelle.gif");
+    $("#behindDoor img").attr("width", "250%");
+
+  } else if(randNum > 0) {
+
+    $("#behindDoor img").attr("src", "assets/images/mountains.jpg");
+    $("#behindDoor img").attr("width", "200%");
+    $("body").css("background-color","white");
+
+  }
 }
