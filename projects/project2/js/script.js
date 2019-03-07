840bc5e83aeb86d9a2d793e90871ebfeb731d95b
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-03-07 15:16:20 -0500

    P2: Added jQuery and responsive voice libraries

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -7,3 +7,27 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+// Get setup
+$(document).ready(setup);
+
+function setup() {
+
+  $('#click-to-begin').on('click',startGame);
+
+}
+
+function startGame() {
+
+    let word = "dog";
+    
+    // Set some random numbers for the voice's pitch and rate parameters for a bit of fun
+    let options = {
+      pitch: Math.random(),
+      rate: Math.random()
+    };
+
+    // Use ResponsiveVoice to speak the string we generated, with UK English Male voice
+    // and the options we just specified.
+    responsiveVoice.speak(word,'UK English Male',options);
+}
