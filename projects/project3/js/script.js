83cb071f5acfecad875c6c96b3532fc2c7531256
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-04-19 17:10:22 -0400

    P3: Added particle effects library

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -7,3 +7,7 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
