04590890a40d771a71e88bb246a7c494ec2eb5ff
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2019-04-22 14:47:12 -0400

    P3: Restructure of code and commenting

--- projects/project3/README.md
+++ projects/project3/README.md
@@ -3,13 +3,24 @@ Project 3: Moodify
 
 <div style="text-align: justify">
 Moodify: Modify the Mood.
-This application sets different kinds of moods with a sense of peacefulness. The goal was to evoke different feelings with its visuals, music, interactivity, and particle effects. The application gives off a vibe of live screensavers but gives the user different levels of interactions. On an overlaying layer, there is a dog with an important task: to wander aimlessly across the different sceneries. I've always felt like dogs were there to accompany people through their different emotive states.
+This application sets different kinds of moods with a sense of peacefulness. The goal was to evoke different feelings with its visuals, music, interactivity, and particle effects. The application gives off a vibe of live screensavers but allows for different levels of interactions. On an overlaying layer, there is a dog with an important task: to wander aimlessly across the different sceneries. Dogs are good at keeping people company through their different emotive states. Therefore, being a bit obsessed with gradients, dogs, and particle effects, it was my goal to generate aesthetically pleasing landscapes.
+The original idea was to make it an ambient sidescroller, focusing on the journey rather than the game.
+Three libraries were used for this project: particles.js, jQuery, and Phaser 3. The different sceneries are made with JSON libraries containing settings which the particles.js reads and applies to the background. jQuery is used to facilitate the interactivity of the menu. Phaser 3 is used for the platformer game overlay. particlesJS has integrated interactivity where mouse hovers and clicks have different effects on the particles. Each setting was customized to fit the different moods.
 
-Being able to generate aesthetically pleasing landscapes using particle effects.
-
-
-Three libraries were used for this project: particles.js, jQuery, and Phaser 3. The different sceneries are made with JSON libraries containing settings which the particles.js reads and applies to the website. jQuery is used to facilitate the interactivity of the menu. Phaser 3 is used for the platformer game overlay. particlesJS has integrated interactivity where mouse hovers and clicks have different effects on the particles. Each setting was customized to fit the different moods.
+Arrow Keys: Right, Left, Up to move
+Use Mouse to interact with menu and background
 
 </div>
 
 __References__
+https://uigradients.com</br>
+https://www.nexusmods.com/stardewvalley/mods/570</br>
+https://github.com/VincentGarreau/particles.js/</br>
+https://fonts.google.com/specimen/Bad+Script</br></br>
+
+__Music__
+Happy: Overworld Theme - Super Mario Land</br>
+Rain: 1 AM Animal Crossing New Leaf - Night Ambiance Sound</br>
+Fall: Stardew Valley OST - Fall (Raven's Descent)</br>
+Snow: Animal Crossing (Winter) - Mario Kart 8 [OST]</br>
+Stars: To the Gateway - Super Mario Galaxy
--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -2,17 +2,24 @@
 
 /*****************
 
-Mood Assistant
+Moodify
 Melissa Lim
 
 ******************/
 
 $(document).ready(setup);
 
-let option;
+// Initialize document variables
+let $html = $('html');
 let windowHeight;
 let windowWidth;
 
+// Initialize variables used for audio management
+let audioIsPlaying = false;
+let playlist = [new Audio("assets/sounds/petals.mp3"),new Audio("assets/sounds/rain.mp3"),new Audio("assets/sounds/leaves.mp3"),new Audio("assets/sounds/snow.mp3"),new Audio("assets/sounds/stars.mp3")];
+let currentScene;
+
+// Initialize platformer game variables
 let platforms;
 let player;
 let cursors;
@@ -37,128 +44,46 @@ let config = {
 };
 let game = new Phaser.Game(config);
 
-let audioIsPlaying = false;
-let playlist = [new Audio("assets/sounds/petals.mp3"),new Audio("assets/sounds/rain.mp3"),new Audio("assets/sounds/leaves.mp3"),new Audio("assets/sounds/snow.mp3"),new Audio("assets/sounds/stars.mp3")];
-let currentScene;
-
+// setup()
+//
+// Assigns variables and calls menu function
 function setup() {
-
   windowHeight = $(window).height();
   windowWidth = $(window).width();
   particlesJS.load('particles-js', 'assets/jsons/particles.json', function() {});
 
-  $('span').on('click', optionClicked);
-
+  $('span').on('click', menu);
 }
 
-function preload() {
-  this.load.image('ground', 'assets/images/platform.png');
-  this.load.spritesheet(
-    'player',
-    'assets/images/player.png',
-    {frameWidth: 32, frameHeight: 45}
-  );
-}
-
-function create() {
-  platforms = this.physics.add.staticGroup();
-
-  platforms.create(windowWidth/2, 1239, 'ground').setScale(10).refreshBody();
-
-  // Adds player sprite
-  player = this.physics.add.sprite(30,1040, 'player');
-
-  // Map-bounded
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-    frameRate: 5,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'player', frame: 4 } ],
-    frameRate: 10
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-    frameRate: 5,
-    repeat: -1
-  });
-
-
-  cursors = this.input.keyboard.createCursorKeys();
-  this.physics.add.collider(player, platforms);
-}
-
-function update() {
-
-  if (cursors.left.isDown)
-  {
-    player.setVelocityX(-140);
-
-    player.anims.play('left', true);
-  }
-  else if (cursors.right.isDown)
-  {
-    player.setVelocityX(140);
-
-    player.anims.play('right', true);
-  }
-  else
-  {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down)
-  {
-    player.setVelocityY(-330);
-  }
-}
-
-
-function optionClicked() {
+// menu()
+//
+// Display menu option changes
+function menu() {
+  // On choice, option menu fades out
   $('#options').fadeOut(1);
   $('#greeting').fadeOut(500);
-  
+  // Back and audio menu fades in
   $('#audio').show();
-
-  $("#audio").on('click', function() {
-    if(audioIsPlaying) {
-      playlist[currentScene].pause();
-      audioIsPlaying = false;
-      this.innerHTML = '<i class="fas fa-volume-mute"></i>';
-    } else {
-
-      playlist[currentScene].play();
-      audioIsPlaying = true;
-      this.innerHTML = '<i class="fas fa-volume-up"></i>';
-    }
-  });
-  // Add back button
+  $('#audio').on('click', audioButton);
   $('#back').show();
-
-  // Fade in menu
-  $("#back").on('click', function() {
+  // When back option chosen, options menu fades back in
+  $('#back').on('click', function() {
     $('#back').fadeOut(50);
-    $('#audio').fadeOut(50);
     $('#options').fadeIn(500);
     $('#greeting').fadeIn(500);
   })
+  // Uses html value of the option that was clicked on to decide which scene to display
+  sceneManager(this.innerHTML);
+}
 
-  option = this.innerHTML;
-
+// sceneManager(option)
+//
+// Takes in argument of option chosen and set up scenes accordingly
+function sceneManager(option) {
   if (option === "Happy") {
     particlesJS.load('particles-js', 'assets/jsons/petals-particles.json', function() {
-      $("html").css("background","linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff)");
-      $("#platformer").css("background","linear-gradient(to top, #56ab2f, #a8e063)");
+      $html.css("background","linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff)");
+      $('#platformer').css("background","linear-gradient(to top, #56ab2f, #a8e063)");
       audioManager(playlist[0]);
       currentScene = 0;
     });
@@ -166,8 +91,8 @@ function optionClicked() {
 
   if (option === "Gloomy") {
     particlesJS.load('particles-js', 'assets/jsons/rain-particles.json', function() {
-      $("html").css("background","linear-gradient(to bottom, #4b79a1, #283e51)");
-      $("#platformer").css("background","linear-gradient(to bottom, #485563, #29323c)");
+      $html.css("background","linear-gradient(to bottom, #4b79a1, #283e51)");
+      $('#platformer').css("background","linear-gradient(to bottom, #485563, #29323c)");
       audioManager(playlist[1]);
       currentScene = 1;
     });
@@ -175,8 +100,8 @@ function optionClicked() {
 
   if (option === "Nostalgic") {
     particlesJS.load('particles-js', 'assets/jsons/leaves-particles.json', function() {
-      $("html").css("background","linear-gradient(to top, #f0cb35, #56ab2f)");
-      $("#platformer").css("background","linear-gradient(to top, #f12711, #f5af19)");
+      $html.css("background","linear-gradient(to top, #f0cb35, #56ab2f)");
+      $('#platformer').css("background","linear-gradient(to top, #f12711, #f5af19)");
       audioManager(playlist[2]);
       currentScene = 2;
     });
@@ -184,8 +109,8 @@ function optionClicked() {
 
   if (option === "Chilly") {
     particlesJS.load('particles-js', 'assets/jsons/snow-particles.json', function() {
-      $("html").css("background","linear-gradient(to bottom, #83a4d4, #b6fbff)");
-      $("#platformer").css("background","linear-gradient(to bottom, #b2fefa, #0ed2f7)");
+      $html.css("background","linear-gradient(to bottom, #83a4d4, #b6fbff)");
+      $('#platformer').css("background","linear-gradient(to bottom, #b2fefa, #0ed2f7)");
       audioManager(playlist[3]);
       currentScene = 3;
     });
@@ -193,14 +118,17 @@ function optionClicked() {
 
   if (option === "Thoughtful") {
     particlesJS.load('particles-js', 'assets/jsons/stars-particles.json', function() {
-      $("html").css("background","linear-gradient(to bottom, #0f2027, #203a43, #2c5364)");
-      $("#platformer").css("background","transparent");
+      $html.css("background","linear-gradient(to bottom, #0f2027, #203a43, #2c5364)");
+      $('#platformer').css("background","transparent");
       audioManager(playlist[4]);
       currentScene = 4;
     });
   }
 }
 
+// audioManager(music)
+//
+// Assigns which song to play
 function audioManager(music) {
   if(!audioIsPlaying) {
     audioIsPlaying = true;
@@ -217,5 +145,100 @@ function audioManager(music) {
     music.loop = true;
     music.volume = 0.1;
     music.play();
+  }
+}
+
+// audioButton()
+//
+// Mute and unmute button
+function audioButton() {
+  if(audioIsPlaying) {
+    playlist[currentScene].pause();
+    audioIsPlaying = false;
+    this.innerHTML = '<i class="fas fa-volume-mute"></i>';
+
+  } else {
+    playlist[currentScene].play();
+    audioIsPlaying = true;
+    this.innerHTML = '<i class="fas fa-volume-up"></i>';
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+//                      Platformer game corner                      //
+//////////////////////////////////////////////////////////////////////
+
+// preload()
+//
+// Loads character and platform images/sprites
+function preload() {
+  this.load.image('ground', 'assets/images/platform.png');
+  this.load.spritesheet(
+    'player',
+    'assets/images/player.png',
+    {frameWidth: 32, frameHeight: 45}
+  );
+}
+
+// create()
+//
+// Adds to scene platforms, character with walking animation, and world physics and boundaries.
+function create() {
+
+  // Adds platform with physics engine
+  platforms = this.physics.add.staticGroup();
+  platforms.create(windowWidth/2, 1239, 'ground').setScale(10).refreshBody();
+
+  // Adds player sprite
+  player = this.physics.add.sprite(30,1040, 'player');
+
+  // Player is map-bounded
+  player.setCollideWorldBounds(true);
+
+  // Takes sprite image and determinte keyframes according to player direction
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+    frameRate: 5,
+    repeat: -1
+  });
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'player', frame: 4 } ],
+    frameRate: 10
+  });
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  // Create variable holding keyboard input
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // Set collision between player and platform
+  this.physics.add.collider(player, platforms);
+}
+
+// update()
+//
+// Update character movement animation and velocity on key press
+function update() {
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-140);
+    player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(140);
+    player.anims.play('right', true);
+  }
+  else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
   }
 }
